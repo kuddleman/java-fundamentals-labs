a5869d07ef4aa3f6baa48af3816d435a98c0a49a
Kuddleman <edl1155@gmail.com>
2020-04-22 09:57:40 -0700

    complete exercise 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -17,6 +17,13 @@ public class Exercise_03 {
         System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
+        System.out.println("Check out my first program!");
+
+        int i = 10;
+        int x = 12;
+        int z = i + x;
+
+        System.out.println(z);
 
     }
 
