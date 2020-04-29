729294b25e9762608d8797339372404bfed87bdb
Kuddleman <edl1155@gmail.com>
2020-04-29 13:28:17 -0700

    complete exercises 1 and 2

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -17,6 +17,15 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int a = 10;
+        double d = 23455643.4566d;
+        float f = 23.4565f;
+        char c = 'c';
+
+        System.out.println("Here is the integer: " + a);
+        System.out.println("Here is the double: " + d);
+        System.out.println("Here is the float: " + f);
+        System.out.println("Here is the char: " + c);
 
     }
 }
--- src/labs_examples/datatypes_operators/labs/Exercise_02.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_02.java
@@ -24,6 +24,13 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // write code here
+        int myInt = 10;
+        double myDb = myInt;
+        System.out.println(myDb);
+
+        double anotherDb = 10.5;
+        int anotherInt = (int) 10.5;
+        System.out.println(anotherInt);
 
     }
 }
