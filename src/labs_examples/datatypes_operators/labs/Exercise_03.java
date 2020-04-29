5f28291f6cbf6c39ab5a46ea001bfe4659f5061e
Kuddleman <edl1155@gmail.com>
2020-04-29 13:43:42 -0700

    complete exercise 3 in datatypes_operators

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,30 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int num1 = 25;
+        int num2 = 6;
+
+        int sum = num1 + num2;
+
+        int difference = num1 - num2;
+
+        int product = num1 * num2;
+
+        int myMod = num1 % num2;
+
+        float float1 = (float) num1;
+        float float2 = (float) num2;
+        float quotient = float1/float2;
+
+
+
+        System.out.println("sum is " + sum);
+        System.out.println("difference is " + difference);
+        System.out.println("product is " + product);
+        System.out.println("quotient is " + quotient);
+        System.out.println("modulus is " + myMod);
+
+
 
     }
 
