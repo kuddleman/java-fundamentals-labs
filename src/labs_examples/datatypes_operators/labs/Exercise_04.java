e40b083d3e83d92785945d78d4b8a25e95796c9c
Kuddleman <edl1155@gmail.com>
2020-04-29 13:52:16 -0700

    complete exercise 4 of datatype_operators

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -18,6 +18,36 @@ class RelationalOperators {
         }
 
         // write your code below
+        int c = 3;
+        int d = 3;
+        if ( c <= d ){
+            System.out.println("c is less than or equal to d");
+        }
+
+        int e = 4;
+        int f = 5;
+        if ( f > e ){
+            System.out.println("f is greater than e");
+        }
+
+        int g = 6;
+        int h = 6;
+        if ( g >= h) {
+            System.out.println("g is greater than  or equal to h");
+        }
+
+        int i = 7;
+        int k = 7;
+        if ( i == k) {
+            System.out.println("i is strictly equal to k");
+        }
+
+        int j = 8;
+        int l = 9;
+        if (j != k) {
+            System.out.println("j is NOT equal to k");
+        }
+
 
     }
 
