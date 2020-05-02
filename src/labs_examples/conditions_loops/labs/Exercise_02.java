71a666692c9545565d97d7fffe48bad0b78ba89e
Kuddleman <edl1155@gmail.com>
2020-05-02 09:49:39 -0700

    complete exercise 2 in conditions lab

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -16,6 +18,31 @@ public class Exercise_02 {
         // 2) prompt user
         // 3) assign input to variable as int
         // 4) write completed code here
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // prompt user
+        System.out.print("Choose a number from 1 to 7 (inclusive) to find out what day that number corresponds to: ");
+        // assign input to variable as int
+        int number = scanner.nextInt();
+
+        if ( number == 1 ) {
+            System.out.println("Today is MONDAY");
+        } else if ( number == 2 ) {
+            System.out.println( "Today is TUESDAY");
+        } else if ( number == 3 ) {
+            System.out.println( "Today is WEDNESDAY");
+        } else if ( number == 4 ) {
+            System.out.println( "Today is THURSDAY" );
+        } else if ( number == 5) {
+            System.out.println( "Today is FRIDAY");
+        } else if ( number == 6 ) {
+            System.out.println( "Today is SATURDAY");
+        } else if ( number == 7 ) {
+            System.out.println( "Today is SUNDAY");
+        } else {
+            System.out.println( "Sorry, I don't know what day it is.");
+        }
 
     }
 }
