b3a1b9e960ca9eac91649315500867b68dece6a5
Kuddleman <edl1155@gmail.com>
2020-05-07 12:25:30 -0700

    complete exercise 4 in arrays

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,23 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+
+        int[][] funny_array = {
+                {5,6,7},
+                {3,5,7,4},
+                {8,9},
+                {19,20,21,22, 23,24}
+        };
+
+        for (int i = 0; i < funny_array.length; i++) {
+            for (int j = 0; j < funny_array[i].length; j++) {
+                System.out.print(funny_array[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+
+    }
 
 }
