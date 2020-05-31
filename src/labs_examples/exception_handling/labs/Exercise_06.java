68cb75fe83c84b9a362acb648fbe28879c4d10f0
Kuddleman <edl1155@gmail.com>
2020-05-31 15:07:09 -0700

    complete exercise 6 in error handling

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -7,3 +7,28 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+  public class Exercise_06 {
+    public static void main(String[] args) {
+      try {
+          methodA();
+      } catch(ArithmeticException ex) {
+          System.out.println("This is an Arithmetic Exception");
+      }
+
+    }
+    public static void methodA() throws ArithmeticException {
+      methodB();
+    }
+    public static void methodB() throws ArithmeticException {
+      int a = 8;
+      int b = 0;
+      try {
+          int result = a / b;
+      } catch (ArithmeticException ex) {
+          System.out.println("exception found from method B");
+          throw ex;
+      }
+
+    }
+}
+
