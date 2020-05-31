98122a1f06f0db7df15b398542b7c424fe2b6d58
Kuddleman <edl1155@gmail.com>
2020-05-31 12:07:59 -0700

    complete ex 1 exceptions

--- src/labs_examples/exception_handling/labs/Exercise_01.java
+++ src/labs_examples/exception_handling/labs/Exercise_01.java
@@ -6,4 +6,24 @@ package labs_examples.exception_handling.labs;
  *      1) Demonstrate a try/catch.
  *
  */
+  public class Exercise_01 {
 
+      public static void main(String[] args) {
+         int num1 = 2;
+         int num2 = 0;
+
+         try {
+             System.out.println(add(num1, num2));
+         } catch(Exception exc) {
+             System.out.println("Please fix error");
+
+         }
+
+          System.out.println("Program completed running");
+      }
+
+      public static int add(int a,  int b) {
+          return a / b;
+
+      }
+}
