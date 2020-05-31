167c970b0371fac1900ff09dbc362dbbaf78304e
Kuddleman <edl1155@gmail.com>
2020-05-31 13:48:35 -0700

    complete exercise 4 exception handling

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,24 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+public class Exercise_03 {
+
+    public static void main(String[] args) {
+        int myArray[] = new int[4];
+        int myInt = 15;
+        try {
+           myArray[34] = myInt;
+            System.out.println("Inside try block");
+        }
+        catch( ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bounds");
+        }
+        finally {
+            System.out.println("FINALLY block");
+        }
+
+
+
+    }
+}
--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,21 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+
+public class Exercise_04 {
+    public static void main(String[] args) {
+
+        try{
+            int myArray[] = { 1, 2, 3, 4, 5, 6 };
+            System.out.println(myArray[1]);
+            try {
+                int x = myArray[3] / 0;
+            } catch( ArithmeticException exc2) {
+                System.out.println("Can't divide by zero");
+            }
+        } catch(ArrayIndexOutOfBoundsException exc1) {
+            System.out.println("Array index is out of bounds");
+        }
+
+    }
+}
