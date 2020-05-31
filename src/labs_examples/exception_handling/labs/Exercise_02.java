3c0df3371878e618b1bcda4f8f5267238715e0bf
Kuddleman <edl1155@gmail.com>
2020-05-31 12:20:56 -0700

    complete ex 2 exception-handling

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -7,4 +7,26 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_02 {
+    public static void main(String[] args) {
+
+        try{
+          int myArray[] = new int[7];
+          myArray[1] = 30 / 0;
+            System.out.println("This is the try block");
+        }
+        catch(ArithmeticException e) {
+            System.out.println("Arithmetic Exception!");
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is out of bounds! NO NO NO");
+        }
+        catch(Exception e) {
+            System.out.println("Some other exception");
+        }
+        System.out.println("Out of try-catch-blocks");
+
+    }
+}
+
 
